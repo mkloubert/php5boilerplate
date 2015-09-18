8e32a2c8e89ad82029a28c1d80d451c0e59653a3
Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
2015-09-18 14:15:49 +0200

    - CHG: arguments for callable action var source

--- sys/classes/php5bp/Modules/ModuleBase.php
+++ sys/classes/php5bp/Modules/ModuleBase.php
@@ -156,7 +156,7 @@ abstract class ModuleBase extends \php5bp\Object implements ModuleInterface {
                 case 'method':
                 case 'callable':
                     $funcRes = \call_user_func_array($var,
-                                                     array(&$found, $result));
+                                                     array(&$found, $ctx, $result));
                     if ($found) {
                         $result = $funcRes;
                     }
